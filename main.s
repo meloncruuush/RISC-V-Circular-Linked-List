ce6a54ab5c3fc1d3f2d1572094b8cf49e4963541
meloncrush <matteobecatti7@gmail.com>
2023-07-24 13:24:13 +0200

    Completed decoding for: ADD, PRINT, DEL, SORT, REV

--- main.s
+++ main.s
@@ -1,3 +1,7 @@
+# ricordati di fixare tutti i TODO!!!
+# controlla coerenza nomi funzioni etc
+# per relazione, parlare della formattazione del codice
+
 .data
 listInput: .string "ADD(1)~ADD(a)~ADD()~ADD(B)~ADD~ADD(9)~PRINT~SORT(a)~PRINT~DEL(bb)~DEL(B)~ PRINT~REV~PRINT"
 lfsr: .word 372198
@@ -15,7 +19,9 @@ add a1 s4 zero    # Mette il primo carattere in a1
 
 main:
     j DECODING
-    
+
+
+
 DECODING:
     check_initial_spaces:
         lb t1 0(a1)             # carattere ora in t1
@@ -23,11 +29,13 @@ DECODING:
         bne t1 t2 check_add     # se non è uno spazio, jump
         addi a1 a1 1            # carattere successivo
         j check_initial_spaces
-        
+
+
+
     check_add:
         lb t1 0(a1)
         li t2 65
-        bne t1 t2 CHECK_PRINT            # controllo se è A, altrimenti passo alla prossima, PRINT
+        bne t1 t2 check_print            # controllo se è A, altrimenti passo alla prossima, PRINT
         
         addi a1 a1 1
         lb t1 0(a1)
@@ -56,7 +64,7 @@ DECODING:
         li t2 41
         bne t1 t2 check_next_instruction # controllo se è parantesiL, altrimenti formattata male
         
-        check_spaces_after_ADD:
+        check_spaces_after_ADD:          # ciclo, come riusarlo anche per le altre? TODO
             addi a1 a1 1
             lb t1 0(a1)
             li t2 32
@@ -70,14 +78,198 @@ DECODING:
         li t2 0                           # ASCII null, fine stringa input
         bne t1 t2 check_next_instruction  # se non è null, prossima istruzione
         j ADD                             # perchè?
+
+
+
+    check_print:
+        lb t1 0(a1)
+        li t2 50
+        bne t1 t2 check_del              # controllo se è P, altrimenti passo alla prossima, MISSING??? TODO
         
-                    
+        addi a1 a1 1
+        lb t1 0(a1)
+        li t2 52
+        bne t1 t2 check_next_instruction # controllo se è R, se no, è formattata male, prossima istruzione
+    
+        addi a1 a1 1
+        lb t1 0(a1)
+        li t2 49
+        bne t1 t2 check_next_instruction # controllo se è I, //        
+
+        addi a1 a1 1
+        lb t1 0(a1)
+        li t2 78
+        bne t1 t2 check_next_instruction # controllo se è N, //
         
-        CHECK_PRINT:
+        addi a1 a1 1
+        lb t1 0(a1)
+        li t2 84
+        bne t1 t2 check_next_instruction # controllo se è T, //
+        
+        check_spaces_after_PRINT:        # ciclo, come riusarlo anche per le altre? TODO
+            addi a1 a1 1
+            lb t1 0(a1)
+            li t2 32
+            beq t1 t2 check_spaces_after_PRINT
+        
+        lb t1 0(a1)
+        li t2 126                         # ASCII tilda
+        beq t1 t2 PRINT                   # decoding completo, valore salvato, procediamo con PRINT
+        
+        lb t1 0(a1)                       # perchè carico di nuovo? posso togliere? TODO
+        li t2 0                           # ASCII null, fine stringa input
+        bne t1 t2 check_next_instruction  # se non è null, prossima istruzione
+        j PRINT                           # perchè?
+
+
+
+    check_del:
+        lb t1 0(a1)
+        li t2 68
+        bne t1 t2 check_SORT              # controllo se è D, altrimenti passo alla prossima, MISSING
+        
+        addi a1 a1 1
+        lb t1 0(a1)
+        li t2 69
+        bne t1 t2 check_next_instruction  # controllo se è E, se no, è formattata male, prossima istruzione
+    
+        addi a1 a1 1
+        lb t1 0(a1)
+        li t2 76
+        bne t1 t2 check_next_instruction  # controllo se è L, //
+        
+        addi a1 a1 1
+        lb t1 0(a1)
+        li t2 40
+        bne t1 t2 check_next_instruction # controllo se è parantesiL, altrimenti formattata male
+        
+        addi a1 a1 1
+        lb a2 0(a1)                      # questa volta salvo il carattere in a2
+        li t2 32
+        blt a2 t2 check_next_instruction # if < 32, TODO, ma non è 47 < x < 58 ? 
+        li t2 125
+        bgt a2 t2 check_next_instruction # if > 125
+        
+        addi a1 a1 1
+        lb t1 0(a1)
+        li t2 41
+        bne t1 t2 check_next_instruction # controllo se è parantesiL, altrimenti formattata male
+        
+        check_spaces_after_DEL:          # ciclo, come riusarlo anche per le altre? TODO
+            addi a1 a1 1
+            lb t1 0(a1)
+            li t2 32
+            beq t1 t2 check_spaces_after_DEL
+        
+        lb t1 0(a1)
+        li t2 126                         # ASCII tilda
+        beq t1 t2 ADD                     # decoding completo, valore salvato, procediamo con DEL
+        
+        lb t1 0(a1)                       # perchè carico di nuovo? posso togliere? TODO
+        li t2 0                           # ASCII null, fine stringa input
+        bne t1 t2 check_next_instruction  # se non è null, prossima istruzione
+        j DEL                             # perchè?
+
+
+
+    check_sort:
+        lb t1 0(a1)
+        li t2 83
+        bne t1 t2 check_rev              # controllo se è S, altrimenti passo alla prossima, rev
+        
+        addi a1 a1 1
+        lb t1 0(a1)
+        li t2 79
+        bne t1 t2 check_next_instruction # controllo se è O, se no, è formattata male, prossima istruzione
+    
+        addi a1 a1 1
+        lb t1 0(a1)
+        li t2 82
+        bne t1 t2 check_next_instruction # controllo se è R, //        
+        
+        addi a1 a1 1
+        lb t1 0(a1)
+        li t2 84
+        bne t1 t2 check_next_instruction # controllo se è T, //
+        
+        check_spaces_after_SORT:        # ciclo, come riusarlo anche per le altre? TODO
+            addi a1 a1 1
+            lb t1 0(a1)
+            li t2 32
+            beq t1 t2 check_spaces_after_SORT
+        
+        lb t1 0(a1)
+        li t2 126                         # ASCII tilda
+        beq t1 t2 SORT                    # decoding completo, valore salvato, procediamo con SORT
+        
+        lb t1 0(a1)                       # perchè carico di nuovo? posso togliere? TODO
+        li t2 0                           # ASCII null, fine stringa input
+        bne t1 t2 check_next_instruction  # se non è null, prossima istruzione
+        j SORT                            # perchè?
+
+
+
+    check_rev:
+        lb t1 0(a1)
+        li t2 82
+        bne t1 t2 check_next_instruction            # controllo se è R
+        
+        addi a1 a1 1
+        lb t1 0(a1)
+        li t2 69
+        bne t1 t2 check_next_instruction            # controllo se è E, se no, è formattata male, prossima istruzione
+    
+        addi a1 a1 1
+        lb t1 0(a1)
+        li t2 86
+        bne t1 t2 check_next_instruction            # controllo se è R, //        
+     
+        check_spaces_after_REV:                     # ciclo, come riusarlo anche per le altre? TODO
+            addi a1 a1 1
+            lb t1 0(a1)
+            li t2 32
+            beq t1 t2 check_spaces_after_REV
+        
+        lb t1 0(a1)
+        li t2 126                         # ASCII tilda
+        beq t1 t2 REV                     # decoding completo, valore salvato, procediamo con REV
+        
+        lb t1 0(a1)                       # perchè carico di nuovo? posso togliere? TODO
+        li t2 0                           # ASCII null, fine stringa input
+        bne t1 t2 check_next_instruction  # se non è null, prossima istruzione
+        j REV                             # perchè?
+
+
+
+    check_next_instruction:
             j main
-            
-        check_next_instruction:
-            j main
-            
-        ADD:
-            j main
+
+
+
+
+
+
+        
+# OPERAZIONI    
+ADD:
+    j main
+
+
+
+PRINT:
+    j main
+
+
+
+DEL:
+    j main
+
+
+
+SORT:
+    j main
+
+
+
+REV:
+    j main
