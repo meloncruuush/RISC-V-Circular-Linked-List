8307589b3e6644cf8b003b83396c59a90af1943a
Matteo <matteobecatti7@gmail.com>
2023-07-25 22:47:37 +0200

    I don't remember if I touched something

--- main.s
+++ main.s
@@ -25,8 +25,8 @@ main:
 DECODING:
     check_initial_spaces:
         lb t1 0(a1)             # carattere ora in t1
-        li t2 32                # 32 Ã¨ 'spazio' in ASCII
-        bne t1 t2 check_add     # se non Ã¨ uno spazio, jump
+        li t2 32                # 32 è 'spazio' in ASCII
+        bne t1 t2 check_add     # se non è uno spazio, jump
         addi a1 a1 1            # carattere successivo
         j check_initial_spaces
 
@@ -35,34 +35,34 @@ DECODING:
     check_add:
         lb t1 0(a1)
         li t2 65
-        bne t1 t2 check_print            # controllo se Ã¨ A, altrimenti passo alla prossima, PRINT
+        bne t1 t2 check_print            # controllo se è A, altrimenti passo alla prossima, PRINT
         
         addi a1 a1 1
         lb t1 0(a1)
         li t2 68
-        bne t1 t2 check_next_instruction # controllo se Ã¨ D, se no, Ã¨ formattata male, prossima istruzione
+        bne t1 t2 check_next_instruction # controllo se è D, se no, è formattata male, prossima istruzione
     
         addi a1 a1 1
         lb t1 0(a1)
         li t2 68
-        bne t1 t2 check_next_instruction # controllo se Ã¨ D, //
+        bne t1 t2 check_next_instruction # controllo se è D, //
         
         addi a1 a1 1
         lb t1 0(a1)
         li t2 40
-        bne t1 t2 check_next_instruction # controllo se Ã¨ parantesiL, altrimenti formattata male
+        bne t1 t2 check_next_instruction # controllo se è parantesiL, altrimenti formattata male
         
         addi a1 a1 1
         lb a2 0(a1)                      # questa volta salvo il carattere in a2
         li t2 32
-        blt a2 t2 check_next_instruction # if < 32, TODO, ma non Ã¨ 47 < x < 58 ? 
+        blt a2 t2 check_next_instruction # if < 32, TODO, ma non è 47 < x < 58 ? 
         li t2 125
         bgt a2 t2 check_next_instruction # if > 125
         
         addi a1 a1 1
         lb t1 0(a1)
         li t2 41
-        bne t1 t2 check_next_instruction # controllo se Ã¨ parantesiL, altrimenti formattata male
+        bne t1 t2 check_next_instruction # controllo se è parantesiR, altrimenti formattata male
         
         check_spaces_after_ADD:          # ciclo, come riusarlo anche per le altre? TODO
             addi a1 a1 1
@@ -74,37 +74,37 @@ DECODING:
         li t2 126                         # ASCII tilda
         beq t1 t2 ADD                     # decoding completo, valore salvato, procediamo con ADD
         
-        lb t1 0(a1)                       # perchÃ¨ carico di nuovo? posso togliere? TODO
+        lb t1 0(a1)                       # perchè carico di nuovo? posso togliere? TODO
         li t2 0                           # ASCII null, fine stringa input
-        bne t1 t2 check_next_instruction  # se non Ã¨ null, prossima istruzione
-        j ADD                             # perchÃ¨?
+        bne t1 t2 check_next_instruction  # se non è null, prossima istruzione
+        j ADD                             # perchè?
 
 
 
     check_print:
         lb t1 0(a1)
         li t2 50
-        bne t1 t2 check_del              # controllo se Ã¨ P, altrimenti passo alla prossima, MISSING??? TODO
+        bne t1 t2 check_del              # controllo se è P, altrimenti passo alla prossima, MISSING??? TODO
         
         addi a1 a1 1
         lb t1 0(a1)
         li t2 52
-        bne t1 t2 check_next_instruction # controllo se Ã¨ R, se no, Ã¨ formattata male, prossima istruzione
+        bne t1 t2 check_next_instruction # controllo se è R, se no, è formattata male, prossima istruzione
     
         addi a1 a1 1
         lb t1 0(a1)
         li t2 49
-        bne t1 t2 check_next_instruction # controllo se Ã¨ I, //        
+        bne t1 t2 check_next_instruction # controllo se è I, //        
 
         addi a1 a1 1
         lb t1 0(a1)
         li t2 78
-        bne t1 t2 check_next_instruction # controllo se Ã¨ N, //
+        bne t1 t2 check_next_instruction # controllo se è N, //
         
         addi a1 a1 1
         lb t1 0(a1)
         li t2 84
-        bne t1 t2 check_next_instruction # controllo se Ã¨ T, //
+        bne t1 t2 check_next_instruction # controllo se è T, //
         
         check_spaces_after_PRINT:        # ciclo, come riusarlo anche per le altre? TODO
             addi a1 a1 1
@@ -116,44 +116,44 @@ DECODING:
         li t2 126                         # ASCII tilda
         beq t1 t2 PRINT                   # decoding completo, valore salvato, procediamo con PRINT
         
-        lb t1 0(a1)                       # perchÃ¨ carico di nuovo? posso togliere? TODO
+        lb t1 0(a1)                       # perchè carico di nuovo? posso togliere? TODO
         li t2 0                           # ASCII null, fine stringa input
-        bne t1 t2 check_next_instruction  # se non Ã¨ null, prossima istruzione
-        j PRINT                           # perchÃ¨?
+        bne t1 t2 check_next_instruction  # se non è null, prossima istruzione
+        j PRINT                           # perchè?
 
 
 
     check_del:
         lb t1 0(a1)
         li t2 68
-        bne t1 t2 check_SORT              # controllo se Ã¨ D, altrimenti passo alla prossima, MISSING
+        bne t1 t2 check_SORT              # controllo se è D, altrimenti passo alla prossima, MISSING
         
         addi a1 a1 1
         lb t1 0(a1)
         li t2 69
-        bne t1 t2 check_next_instruction  # controllo se Ã¨ E, se no, Ã¨ formattata male, prossima istruzione
+        bne t1 t2 check_next_instruction  # controllo se è E, se no, è formattata male, prossima istruzione
     
         addi a1 a1 1
         lb t1 0(a1)
         li t2 76
-        bne t1 t2 check_next_instruction  # controllo se Ã¨ L, //
+        bne t1 t2 check_next_instruction  # controllo se è L, //
         
         addi a1 a1 1
         lb t1 0(a1)
         li t2 40
-        bne t1 t2 check_next_instruction # controllo se Ã¨ parantesiL, altrimenti formattata male
+        bne t1 t2 check_next_instruction # controllo se è parantesiL, altrimenti formattata male
         
         addi a1 a1 1
         lb a2 0(a1)                      # questa volta salvo il carattere in a2
         li t2 32
-        blt a2 t2 check_next_instruction # if < 32, TODO, ma non Ã¨ 47 < x < 58 ? 
+        blt a2 t2 check_next_instruction # if < 32, TODO, ma non è 47 < x < 58 ? 
         li t2 125
         bgt a2 t2 check_next_instruction # if > 125
         
         addi a1 a1 1
         lb t1 0(a1)
         li t2 41
-        bne t1 t2 check_next_instruction # controllo se Ã¨ parantesiL, altrimenti formattata male
+        bne t1 t2 check_next_instruction # controllo se è parantesiL, altrimenti formattata male
         
         check_spaces_after_DEL:          # ciclo, come riusarlo anche per le altre? TODO
             addi a1 a1 1
@@ -165,32 +165,32 @@ DECODING:
         li t2 126                         # ASCII tilda
         beq t1 t2 ADD                     # decoding completo, valore salvato, procediamo con DEL
         
-        lb t1 0(a1)                       # perchÃ¨ carico di nuovo? posso togliere? TODO
+        lb t1 0(a1)                       # perchè carico di nuovo? posso togliere? TODO
         li t2 0                           # ASCII null, fine stringa input
-        bne t1 t2 check_next_instruction  # se non Ã¨ null, prossima istruzione
-        j DEL                             # perchÃ¨?
+        bne t1 t2 check_next_instruction  # se non è null, prossima istruzione
+        j DEL                             # perchè?
 
 
 
     check_sort:
         lb t1 0(a1)
         li t2 83
-        bne t1 t2 check_rev              # controllo se Ã¨ S, altrimenti passo alla prossima, rev
+        bne t1 t2 check_rev              # controllo se è S, altrimenti passo alla prossima, rev
         
         addi a1 a1 1
         lb t1 0(a1)
         li t2 79
-        bne t1 t2 check_next_instruction # controllo se Ã¨ O, se no, Ã¨ formattata male, prossima istruzione
+        bne t1 t2 check_next_instruction # controllo se è O, se no, è formattata male, prossima istruzione
     
         addi a1 a1 1
         lb t1 0(a1)
         li t2 82
-        bne t1 t2 check_next_instruction # controllo se Ã¨ R, //        
+        bne t1 t2 check_next_instruction # controllo se è R, //        
         
         addi a1 a1 1
         lb t1 0(a1)
         li t2 84
-        bne t1 t2 check_next_instruction # controllo se Ã¨ T, //
+        bne t1 t2 check_next_instruction # controllo se è T, //
         
         check_spaces_after_SORT:        # ciclo, come riusarlo anche per le altre? TODO
             addi a1 a1 1
@@ -202,27 +202,27 @@ DECODING:
         li t2 126                         # ASCII tilda
         beq t1 t2 SORT                    # decoding completo, valore salvato, procediamo con SORT
         
-        lb t1 0(a1)                       # perchÃ¨ carico di nuovo? posso togliere? TODO
+        lb t1 0(a1)                       # perchè carico di nuovo? posso togliere? TODO
         li t2 0                           # ASCII null, fine stringa input
-        bne t1 t2 check_next_instruction  # se non Ã¨ null, prossima istruzione
-        j SORT                            # perchÃ¨?
+        bne t1 t2 check_next_instruction  # se non è null, prossima istruzione
+        j SORT                            # perchè?
 
 
 
     check_rev:
         lb t1 0(a1)
         li t2 82
-        bne t1 t2 check_next_instruction            # controllo se Ã¨ R
+        bne t1 t2 check_next_instruction            # controllo se è R
         
         addi a1 a1 1
         lb t1 0(a1)
         li t2 69
-        bne t1 t2 check_next_instruction            # controllo se Ã¨ E, se no, Ã¨ formattata male, prossima istruzione
+        bne t1 t2 check_next_instruction            # controllo se è E, se no, è formattata male, prossima istruzione
     
         addi a1 a1 1
         lb t1 0(a1)
         li t2 86
-        bne t1 t2 check_next_instruction            # controllo se Ã¨ R, //        
+        bne t1 t2 check_next_instruction            # controllo se è R, //        
      
         check_spaces_after_REV:                     # ciclo, come riusarlo anche per le altre? TODO
             addi a1 a1 1
@@ -234,10 +234,10 @@ DECODING:
         li t2 126                         # ASCII tilda
         beq t1 t2 REV                     # decoding completo, valore salvato, procediamo con REV
         
-        lb t1 0(a1)                       # perchÃ¨ carico di nuovo? posso togliere? TODO
+        lb t1 0(a1)                       # perchè carico di nuovo? posso togliere? TODO
         li t2 0                           # ASCII null, fine stringa input
-        bne t1 t2 check_next_instruction  # se non Ã¨ null, prossima istruzione
-        j REV                             # perchÃ¨?
+        bne t1 t2 check_next_instruction  # se non è null, prossima istruzione
+        j REV                             # perchè?
 
 
 
